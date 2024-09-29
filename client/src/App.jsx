fdb3a536558e71a7123db28936f11dafa4ec6b38
PranavBj2406 <pranavbj80@gmail.com>
2024-09-29 19:45:41 +0530

    Added Pages to our website

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Signin from "./pages/signin";
+import Signup from "./pages/signup";
 
 export default function App() {
   return (
-   <h1 className='text-red-500'>APP</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/About" element={<About/>} />
+        <Route path="/profile" element={<Profile/>} />
+        <Route path="/signin" element={<Signin/>} />
+        <Route path="/signup" element={<Signup/>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
