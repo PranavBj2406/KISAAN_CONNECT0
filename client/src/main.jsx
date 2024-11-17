0085bf27f2e4467349d1ac4be022687c8f4ff84d
PranavBj2406 <pranavbj80@gmail.com>
2024-11-17 22:14:33 +0530

    added redux-persist :( shits going down

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,19 @@
-import { StrictMode } from 'react';
+
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { store } from './redux/store.js';
+import { persistor, store } from './redux/store.js';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
       <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
