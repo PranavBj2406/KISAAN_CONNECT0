794b07fcb19ab8a5be5cfa3cf99b561e70547c93
PranavBj2406 <pranavbj80@gmail.com>
2024-11-22 22:54:44 +0530

    added new components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, useLocation, Outlet } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation,
+  Outlet,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
@@ -8,38 +14,48 @@ import Signup from "./pages/Signup";
 import Header from "./components/Header";
 import Header2 from "./components/Header2";
 import ContactUs from "./pages/Contactus";
-import 'typeface-poppins';
+
+import "typeface-poppins";
 import Dashboard from "./pages/Dashboard";
 import PrivateRoutes from "./components/PrivateRoutes";
 import SideBar from "./components/SideBar";
- import { SideBarProvider } from "./components/SideBar";
+import { SideBarProvider } from "./components/SideBar";
+import Marketplace from "./pages/Marketplace";
+import Orders from "./pages/Orders";
+import Contracts from "./pages/Contracts";
+import Analysis from "./pages/Analysis";
 
 const header2Routes = [
   "/dashboard",
   "/contactus",
+  "/marketplace",
+  "/orders",
+  "/contracts",
+  "/profile",
+  "/analysis",
   // Add future routes here
   // You can also use pattern matching if needed
   // "/admin/*",    // Example: all admin routes
   // "/settings/*"  // Example: all settings routes
 ];
 
-function ProtectedLayout(){
-  return(
+function ProtectedLayout() {
+  return (
     <div className="flex">
-      <SideBar/>
+      <SideBar />
       <div className="flex-1">
-      <Outlet/>
+        <Outlet />
       </div>
     </div>
-  )
+  );
 }
 
 function AppContent() {
   const location = useLocation(); // Correct use of useLocation
 
-  const showHeader2 = header2Routes.some(route => 
-    route.endsWith('*') 
-      ? location.pathname.startsWith(route.replace('*', ''))
+  const showHeader2 = header2Routes.some((route) =>
+    route.endsWith("*")
+      ? location.pathname.startsWith(route.replace("*", ""))
       : location.pathname === route
   );
 
@@ -53,12 +69,16 @@ function AppContent() {
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
 
-        
-        <Route element={<PrivateRoutes/>}>
-        <Route element={<ProtectedLayout/>}>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/contactus" element={<ContactUs />} />
-        </Route>
+        <Route element={<PrivateRoutes />}>
+          <Route element={<ProtectedLayout />}>
+            <Route path="/marketplace" element={<Marketplace />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/contracts" element={<Contracts />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/contactus" element={<ContactUs />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/analysis" element={<Analysis />} />
+          </Route>
         </Route>
       </Routes>
     </div>
@@ -69,7 +89,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <SideBarProvider>
-      <AppContent />
+        <AppContent />
       </SideBarProvider>
     </BrowserRouter>
   );
